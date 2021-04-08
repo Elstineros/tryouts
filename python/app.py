2bf1d0531520c140236b59f0b2245ad6730ed296
Elstin <elstineros@gmail.com>
2021-04-08 19:33:56 +0530

    update

--- python/app.py
+++ python/app.py
@@ -8,10 +8,15 @@ from mpu6050 import mpu6050
 import csv
 from RaspberryMotors.motors import servos
 import RPi.GPIO as GPIO
+import pigpio
 
+GPIO.setwarnings(False)
 servos.setMode(GPIO.BCM)
-
+# GPIO.setup(12,GPIO.OUT)
+# p=GPIO.PWM(12,100)
+# p.start(0)
 app = Flask(__name__)
+pig = pigpio.pi()
 
 mpu = mpu6050(0x68)
 
@@ -46,27 +51,25 @@ def data():
 @app.route('/steering', methods=["GET", "POST"])
 def steering():
     steeringInput = request.args.get('steeringInput')
+
     print("steeringInput : "+steeringInput)
     servos.ResetGpioAtShutdown(False)
-    
-    # servos.setMode(GPIO.BCM) # refer to the pins by the "Broadcom SOC channel" number
-    # servos.ResetGpioAtShutdown(False) # do not reset GPIO at last servo shutdown
+    #pig.set_servo_pulsewidth(12, steeringInput)
+    servos.setMode(GPIO.BCM) # refer to the pins by the "Broadcom SOC channel" number
+    servos.ResetGpioAtShutdown(False) # do not reset GPIO at last servo shutdown
     s1 = servos.servo(23)
     s1.setAngleAndWait(steeringInput, 0.5) # move to position of 180 degrees
     s1.shutdown();
-    GPIO.cleanup()
+    #GPIO.cleanup()
    
     return steeringInput
 
 @app.route('/throttle', methods=["GET", "POST"])
 def throttle():
-    GPIO.setup(18, GPIO.OUT)
-   
-
     throttleInput = request.args.get('throttleInput')
     print("throttleInput : "+throttleInput)
-    p=GPIO.PWM(18,50)
-    p.ChangeDutyCycle(int(throttleInput))
+    pig.set_servo_pulsewidth(12, throttleInput)
+    
     return throttleInput
 
 if __name__ == "__main__":
